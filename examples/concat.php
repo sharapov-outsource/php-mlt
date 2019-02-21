767da7340137955350e3f282c6fbe5ff80fd96e8
Alexander S <alexander@sharapov.biz>
2019-02-21 21:14:19 +0600

    Initial commit

---
+++ examples/concat.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Sharapov A. <alexander@sharapov.biz>
+ * @link      http://www.sharapov.biz/
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
+ * Date: 2019-02-21
+ * Time: 21:08
+ */
+
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+date_default_timezone_set('UTC');
+
